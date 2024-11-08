36e90d27b3ffdb90d2662d09c43a5658a88579dd
Asha <asha.gasimova@gmail.com>
2024-11-08 11:52:09 +0400

    Homework in place

--- src/task2If.java
+++ src/task2If.java
@@ -5,7 +5,7 @@ public class task2If {
         Scanner scanner = new Scanner(System.in);
         System.out.println("eded daxil edin");
         var a = scanner.nextInt();
-        if (a%3==0 && a%4==0){
+        if (a%3==0 || a%4==0){
             System.out.println("eded 3-e ve ya 4-e bolunur.");
         }else {
             System.out.println("eded 3-e ve 4-e bolunmur");
