10bf2e86c48f88a5f33584de09bc01cb82b49f4a
Asha <asha.gasimova@gmail.com>
2024-11-09 20:50:50 +0400

    Homework in place

--- src/task6.java
+++ src/task6.java
@@ -6,17 +6,21 @@ public class task6 {
         System.out.print("Odeyeceyiniz meblegi daxil edin: ");
         var umumiMebleg = scanner.nextDouble();
         if (100 <= umumiMebleg && 300 >= umumiMebleg) {
-            System.out.println("5% endirim qazandiniz! Sizin odeyeceyiniz mebleg:  "
-                    + (umumiMebleg - (0.05 * umumiMebleg))+"AZN");
+            System.out.println("5% endirim qazandiniz!\nUmumi mebleg = " + umumiMebleg+ " AZN" +
+                    "\nSizin odeyeceyiniz mebleg: "
+                    + (umumiMebleg - (0.05 * umumiMebleg))+" AZN");
         } else if (300 < umumiMebleg && 500 >= umumiMebleg) {
-            System.out.println("10% endirim qazandiniz! Sizin odeyeceyiniz mebleg:  "
-                    + (umumiMebleg - (0.1 * umumiMebleg))+"AZN");
+            System.out.println("10% endirim qazandiniz!\nUmumi mebleg = "+ umumiMebleg + " AZN" +
+                    "\nSizin odeyeceyiniz mebleg:"
+                    + (umumiMebleg - (0.1 * umumiMebleg))+" AZN");
         }else if (500 < umumiMebleg && 800 >= umumiMebleg) {
-            System.out.println("15% endirim qazandiniz! Sizin odeyeceyiniz mebleg:   "
-                    + (umumiMebleg - (0.15 * umumiMebleg)) + "AZN");
+            System.out.println("15% endirim qazandiniz!\nUmumi mebleg = " + umumiMebleg + " AZN" +
+                    "\nSizin odeyeceyiniz mebleg: "
+                    + (umumiMebleg - (0.15 * umumiMebleg)) + " AZN");
         } else if (800 < umumiMebleg && 1000 >= umumiMebleg) {
-            System.out.println("20% endirim qazandiniz! Sizin odeyeceyiniz mebleg:  "
-                    + (umumiMebleg - (0.2 * umumiMebleg))+"AZN");
+            System.out.println("20% endirim qazandiniz!\nUmumi mebleg = " + umumiMebleg + " AZN" +
+                    "\nSizin odeyeceyiniz mebleg: "
+                    + (umumiMebleg - (0.2 * umumiMebleg))+" AZN");
         }else {
             System.out.println("Sevh secim");
         }
