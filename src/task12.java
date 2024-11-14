b8fec60bd1f53b67e22fb980396b08638b3a0d3c
Asha <asha.gasimova@gmail.com>
2024-11-14 23:43:14 +0400

    Homework in place

--- src/task12.java
+++ src/task12.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class task12 {
     public static void main(String[] args) {
-        System.out.print("Iki reqemli eded daxil edin: ");
-        Scanner scanner = new Scanner(System.in);
-        int eded = scanner.nextInt();
-            System.out.println((eded/10) + " " + (eded%10));
+            System.out.print("Iki reqemli eded daxil edin: ");
+            Scanner scanner = new Scanner(System.in);
+            int eded = scanner.nextInt();
+            if (eded > 9 && eded < 100) {
+                System.out.println((eded / 10) + " " + (eded % 10));
+            } else {
+                System.out.println("eded iki reqemli deyil");
+
+            }
+        }
     }
 
-}
