04a002182c76e3ef555858598aee01cde20458e6
Asha <asha.gasimova@gmail.com>
2024-11-14 20:25:50 +0400

    Homework in place

--- src/task12.java
+++ src/task12.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class task12 {
+    public static void main(String[] args) {
+        System.out.print("Iki reqemli eded daxil edin: ");
+        Scanner scanner = new Scanner(System.in);
+        int eded = scanner.nextInt();
+            System.out.println((eded/10) + " " + (eded%10));
+
+
+
+
+
+
+    }
+
 }
