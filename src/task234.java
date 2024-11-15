2271458d0971dd49c6c2dea106b699cedc0580c4
Asha <asha.gasimova@gmail.com>
2024-11-15 19:57:15 +0400

    Homework in place

---
+++ src/task234.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class task234 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int cem = 0;
+        while (true) {
+            int eded = scanner.nextInt();
+            cem+=eded;
+                if (eded == 0) {
+                    System.out.println("program bitdi");
+                    System.out.println(cem);
+                    break;
+                }
+            }
+
+        }
+    }
