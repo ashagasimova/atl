4daf09dd0f6a9692e7fa08c74f33e358316c0bea
Asha <asha.gasimova@gmail.com>
2024-11-15 19:11:05 +0400

    Homework in place

---
+++ src/doWhiletask.java
@@ -0,0 +1,12 @@
+public class doWhiletask {
+    public static void main(String[] args) {
+        int i = 1;
+        int cem = 0;
+        do {
+            cem+=i;
+            i++;
+        }
+        while (i<=10);
+        System.out.println(cem);
+    }
+}
