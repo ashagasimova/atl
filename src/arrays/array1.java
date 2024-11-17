839057c031dd0a69d638c642c7ed52e7ea15e06a
Asha <asha.gasimova@gmail.com>
2024-11-17 19:04:39 +0400

    Homework in place

---
+++ src/arrays/array1.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public static void main(String[] args) {
+        int[] arr = new int[5];
+        Scanner scanner = new Scanner(System.in);
+        arr[0] = scanner.nextInt();
+        arr[1] = scanner.nextInt();
+        arr[2] = scanner.nextInt();
+        arr[3] = scanner.nextInt();
+        arr[4] = scanner.nextInt();
+        System.out.println(Arrays.toString(arr));
+}
