61d3e85f2560ded83c281974fabaee6db0ca0ef5
Asha <asha.gasimova@gmail.com>
2024-11-17 19:19:28 +0400

    Homework in place

--- src/arrays/array1.java
+++ src/arrays/array1.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 public static void main(String[] args) {
         int[] arr = new int[5];
         Scanner scanner = new Scanner(System.in);
-        arr[0] = scanner.nextInt();
-        arr[1] = scanner.nextInt();
-        arr[2] = scanner.nextInt();
-        arr[3] = scanner.nextInt();
-        arr[4] = scanner.nextInt();
+        System.out.println("arrayin elementlerin daxil edin");
+        for (var i=0 ; i<arr.length; i++)
+                arr[i]= scanner.nextInt();
         System.out.println(Arrays.toString(arr));
 }
