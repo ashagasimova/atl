4b1b73f91a02a2b97ba70bcbc3d97cf4d0286750
Asha <asha.gasimova@gmail.com>
2024-11-19 00:24:19 +0400

    Homework in place

--- src/methodTask/Main.java
+++ src/methodTask/Main.java
@@ -1,10 +1,12 @@
+package methodTask;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String myText = "hello";
-        System.out.println(myText.toUpperCase());
-
+        Task1 quvvet = new Task1();
+        int netice = quvvet.quvvetHesablama();
+        System.out.println(netice);
 
     }
 }
