60cf65d94dddc7e25200d91348c7b8c527814b91
Asha <asha.gasimova@gmail.com>
2024-12-24 15:20:02 +0400

    Initial commit

--- src/Resource/FileR.java
+++ src/Resource/FileR.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 
 public class FileR {
     public static void main(String[] args) throws IOException {
-        FileWriter writer = new FileWriter("research.txt");
+        FileWriter writer = new FileWriter("C:\\Users\\ashag\\IdeaProjects\\Atl Project\\src\\Resource\\resource");
         writer.write("Asha Gasimova");
         writer.close();
 
-        FileReader reader = new FileReader("research.txt");
+        FileReader reader = new FileReader("C:\\Users\\ashag\\IdeaProjects\\Atl Project\\src\\Resource\\resource");
         int data;
         while ((data = reader.read())!=-1){
             System.out.print((char)data);
